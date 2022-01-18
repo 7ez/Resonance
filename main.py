47fe9f0cd7d29480fa0e94e36f6ad0791da5b7dc
Aochi <aochi@fuquila.net>
2022-01-18 23:58:47 +0100

    bump ver

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from objects import glob
 from helpers.logger import info, error
 
 app = Xevel(glob.config.socket)
-glob.version = Version(0, 0, 7)
+glob.version = Version(0, 0, 8)
 
 @app.before_serving()
 async def connect() -> None:
