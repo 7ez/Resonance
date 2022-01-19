e73180aa95ae457fe6ee242f8ff7af9f44a98879
Aochi <aochi@fuquila.net>
2022-01-19 11:46:58 +0100

    incr ver

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from objects import glob
 from helpers.logger import info, error
 
 app = Xevel(glob.config.socket)
-glob.version = Version(0, 1, 0)
+glob.version = Version(0, 1, 1)
 
 @app.before_serving()
 async def connect() -> None:
